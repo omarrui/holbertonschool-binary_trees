b608018a3fa2b7ba3796f9ef8852504980ffd483
nawfel83 <laklitnawfel1@gmail.com>
2025-04-09 11:42:20 +0200

    Fix binary_tree_height function to return -1 for NULL tree instead of 0

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,10 +8,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	int left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
